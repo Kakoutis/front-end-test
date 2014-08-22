fde96932631ba36be24f1f67a84b7420eaf0e138
kakoutis Dimitris <d.kakoutis@gmail.com>
2014-08-22 17:39:03 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,54 @@
+/* ==========================================================================
+
+    Project: front end test
+    Author: XHTMLized
+    Last updated: @@timestamp
+
+   ========================================================================== */
+
+(function($) {
+
+    'use strict';
+
+    var App = {
+
+        /**
+         * Init Function
+         */
+        init: function() {
+            // App.feature1();
+            // App.feature2();
+        },
+
+        /**
+         * Custom feature 1
+         */
+        feature1: function() {
+
+        },
+
+        /**
+         * Custom feature 2
+         */
+        feature2: function() {
+
+        }
+
+    };
+
+    $(function() {
+        App.init();
+    });
+
+    $('.gallery').colorbox({
+        open: true,
+        rel: 'group1'
+    });
+
+    $(document).bind('cbox_complete', function() {
+        setTimeout($.colorbox.next, 2000 );
+        setTimeout(parent.$.colorbox.close, 8000);
+    });
+
+
+})(jQuery);
